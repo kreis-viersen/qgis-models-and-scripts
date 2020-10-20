ffd792d8a215716bad400fba63be9cef78cead2c
pathmapper <pathmapper@posteo.de>
2020-10-20 09:40:32 +0200

    Add functions _all_field and _array_difference

---
+++ functions/_all_fields.py
@@ -0,0 +1,14 @@
+from qgis.core import *
+from qgis.gui import *
+
+@qgsfunction(args='auto', group='Custom')
+def all_fields(layer, feature, parent):
+    """
+    Erstellt ein Array mit den Feldern(Spalten) eines Layers
+    <br>
+    <h2>Syntax</h2>
+    all_fields(layer)
+    """
+    provider = layer.dataProvider()
+    field_names = [field.name() for field in provider.fields()]
+    return field_names
